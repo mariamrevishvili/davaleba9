51a2d2868dd5ac4c5a2910a4d64ab1987237f6dc
mariamrevishvili <140517621+mariamrevishvili@users.noreply.github.com>
2023-08-17 03:00:35 +0400

    Add files via upload

---
+++ davaleba9.py
@@ -0,0 +1,7 @@
+#age input
+age=int(input("how old are you: "))
+
+for i in range(8):
+   print("congratulations, you turned:",i,"years")
+for i in range(8,age=1):
+   print("congratulations, you have come to goa")
